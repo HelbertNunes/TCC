8d25ba833095e5d085706944c654beda11d091eb
Helbert <helbertnunes97@gmail.com>
2019-09-06 02:27:08 -0300

    Commit inicial do repositórios

---
+++ Downloader/DownloadComment.js
@@ -0,0 +1,47 @@
+var gplay = require('google-play-scraper');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+
+function adicionaAplicativoCategoria(array, app, categoria) {     
+    array.forEach(element => {
+        element.aplicativo = app;
+        element.categoria = categoria;
+    });
+}
+
+function filtraJson(jsonObj, i) {
+    var str = 'Assassins Creed Identity';
+    jsonObj.aplicativo = 'Assassins Creed Identity';
+    jsonObj.categoria = 'Ação';
+    adicionaAplicativoCategoria(jsonObj, 'Assassins Creed Identity', 'Ação');
+    const csvWriter = createCsvWriter({
+        path: str.concat(i, '.csv'),
+        header: [
+            { id: 'userName', title: 'USUARIO' },
+            { id: 'text', title: 'COMENTARIO' },
+            { id: 'date', title: 'DATACOMENTARIO' },
+            { id: 'aplicativo', title: 'APLICATIVO' },
+            { id: 'categoria', title: 'CATEGORIA' }
+        ]
+    });
+    csvWriter.writeRecords(jsonObj)       // returns a promise
+        .then(() => {
+            console.log('...Done');
+        });
+}
+function coletadorDeComentarios(i) {
+    gplay.reviews({
+        appId: 'com.ubisoft.assassinscreed.identity',
+        page: i,
+        lang: 'en'
+    }).then(function (comentarios) { filtraJson(comentarios, i) }, console.log);
+}
+
+function rodaNvezes(n) {
+    for (var i = 0; i < n; i++) {
+        coletadorDeComentarios(i);
+    }
+}
+rodaNvezes(20);
+
+
