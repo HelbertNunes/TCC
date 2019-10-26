f4010c7d4431f2b6b615c96bf7940586a106d065
Helbert <helbertnunes97@gmail.com>
2019-10-26 05:04:09 -0300

    alterações 26-10

--- Analisador/polaridadeComentarios.py
+++ Analisador/polaridadeComentarios.py
@@ -6,13 +6,16 @@ comentarios = open("Z:\Playgroud\TCC\ComentariosSeparados.txt", "r", encoding="u
 
 arquivoComentariosPositivos = open("Z:\Playgroud\TCC\ComentariosPositivos.txt", "w" ,encoding="utf8")
 arquivoComentariosNegativos = open("Z:\Playgroud\TCC\ComentariosNegativos.txt", "w", encoding="utf8")
+arquivoComentariosNeutros = open("Z:\Playgroud\TCC\ComentariosNeutros.txt", "w", encoding="utf8")
 
 def separaPolaridadeComentarios(sentence):
     score = analyser.polarity_scores(sentence)
     if score['neg'] > 0.5:
         arquivoComentariosNegativos.write(sentence)
     elif score['pos'] > 0.5:
-        arquivoComentariosPositivos.write( sentence)  
+        arquivoComentariosPositivos.write(sentence)
+    elif score['neu'] > 0.5:
+        arquivoComentariosNeutros.write(sentence)  
 
 
 for line in comentarios:    
@@ -20,4 +23,5 @@ for line in comentarios:
     
 arquivoComentariosPositivos.close()
 arquivoComentariosNegativos.close()
+arquivoComentariosNeutros.close()
     
